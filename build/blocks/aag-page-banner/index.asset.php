99242098b701781f7c17ecb0acbe56857be6c90b
Gabriel Coronado <gcoronado5100@gmail.com>
2024-07-02 13:05:42 -0600

    style[mobile] Changed the style on the page banner block

--- build/blocks/aag-page-banner/index.asset.php
+++ build/blocks/aag-page-banner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '80e76f9bec44a8325b0c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1b50947f0646bf43f608');
